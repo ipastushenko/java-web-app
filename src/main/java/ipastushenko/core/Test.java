0530520c6582ef2761db69fb2bd8a9835b671645
ipastushenko <pastushenko.ivan@gmail.com>
2014-04-03 20:42:19 +0700

    delete manifest file

--- src/main/java/ipastushenko/core/Test.java
+++ src/main/java/ipastushenko/core/Test.java
@@ -5,6 +5,6 @@ package ipastushenko.core;
  */
 public class Test {
     public static void main(String [] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World!");
     }
 }
