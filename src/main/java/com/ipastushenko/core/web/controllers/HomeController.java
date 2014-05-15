dea5ded2bb4a9eba664bbc477311c12327a38f56
ipastushenko <pastushenko.ivan@gmail.com>
2014-05-16 02:03:05 +0700

    add correct servlet for spring

--- src/main/java/com/ipastushenko/core/web/controllers/HomeController.java
+++ src/main/java/com/ipastushenko/core/web/controllers/HomeController.java
@@ -2,14 +2,16 @@ package com.ipastushenko.core.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * home controller
  */
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String home() {
-        return "index";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView home() {
+        return new ModelAndView("index");
     }
 }
