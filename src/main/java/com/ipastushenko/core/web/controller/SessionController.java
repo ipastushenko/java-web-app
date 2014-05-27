7f5ec423ca3ebb865ac92ddcddae6ffd77b02691
ipastushenko <pastushenko.ivan@gmail.com>
2014-05-27 12:01:58 +0700

    rename package

--- src/main/java/com/ipastushenko/core/web/controller/SessionController.java
+++ src/main/java/com/ipastushenko/core/web/controller/SessionController.java
@@ -1,4 +1,4 @@
-package com.ipastushenko.core.web.controllers;
+package com.ipastushenko.core.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
