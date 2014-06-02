a6281b2972ab77b57e64c62b93b8a135f4a38b09
ipastushenko <pastushenko.ivan@gmail.com>
2014-06-02 21:55:00 +0700

    refactoring mappers: annotations->xml

--- src/main/java/com/ipastushenko/core/repository/mapper/UserDetailsMapper.java
+++ src/main/java/com/ipastushenko/core/repository/mapper/UserDetailsMapper.java
@@ -1,19 +1,15 @@
 package com.ipastushenko.core.repository.mapper;
 
 import com.ipastushenko.core.model.UserDetailsImpl;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
 
 /**
  * user details mapper
  */
 public interface UserDetailsMapper {
-    @Select("SELECT u.username as u_username, u.password as u_password, r.name as r_name, r.id as r_id " +
-            "FROM users u " +
-            "   LEFT OUTER JOIN users_roles ur ON u.id = ur.user_id " +
-            "   LEFT OUTER JOIN roles r ON ur.role_id = r.id " +
-            "WHERE u.username=#{username}")
-    @ResultMap(value = "UserDetailsMap")
-    UserDetailsImpl findByUsername(@Param("username") String username);
+    /**
+     * find user details by username
+     * @param username userbane
+     * @return user or null
+     */
+    UserDetailsImpl findByUsername(String username);
 }
--- src/main/java/com/ipastushenko/core/repository/mapper/UserMapper.java
+++ src/main/java/com/ipastushenko/core/repository/mapper/UserMapper.java
@@ -1,17 +1,15 @@
 package com.ipastushenko.core.repository.mapper;
 
 import com.ipastushenko.core.model.User;
-import org.apache.ibatis.annotations.*;
 
 /**
  * user mapper
  */
 public interface UserMapper {
-    @Select("SELECT * FROM users WHERE id=#{id}")
-    @Results({
-            @Result(column = "id", property = "id"),
-            @Result(column = "username", property = "username"),
-            @Result(column = "password", property = "password")
-    })
-    User find(@Param("id") Long id);
+    /**
+     * find user by id
+     * @param id id
+     * @return User or null
+     */
+    User find(Long id);
 }
