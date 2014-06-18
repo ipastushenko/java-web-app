0300f8dc6b4f3cb1ef8a28a95bec8e1788d815c4
ipastushenko <pastushenko.ivan@gmail.com>
2014-06-18 23:22:38 +0700

    add migration for registration

---
+++ sql_down/V2.0_pg_add_fields_for_registration.sql
@@ -0,0 +1,6 @@
+ALTER TABLE users DROP COLUMN created_at;
+ALTER TABLE users DROP COLUMN updated_at;
+ALTER TABLE users DROP COLUMN first_name;
+ALTER TABLE users DROP COLUMN last_name;
+ALTER TABLE users DROP COLUMN last_ip;
+ALTER TABLE users DROP COLUMN last_login;
