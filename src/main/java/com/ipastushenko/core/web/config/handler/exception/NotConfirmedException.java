ea247e53a36c83790e3dab4812ab4e41e19b2f33
ipastushenko <pastushenko.ivan@gmail.com>
2014-07-10 00:13:48 +0700

    refactoring: change parent for not confirmed exception

--- src/main/java/com/ipastushenko/core/web/config/handler/exception/NotConfirmedException.java
+++ src/main/java/com/ipastushenko/core/web/config/handler/exception/NotConfirmedException.java
@@ -1,11 +1,11 @@
 package com.ipastushenko.core.web.config.handler.exception;
 
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.AuthenticationException;
 
 /**
  * non confirmed exception
  */
-public class NotConfirmedException extends UsernameNotFoundException {
+public class NotConfirmedException extends AuthenticationException {
     private static final long serialVersionUID = -3841385373761119562L;
 
     public NotConfirmedException(String msg) {
