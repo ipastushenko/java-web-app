4e91a7836df346d99239450c48320baf5bdd02a8
ipastushenko <pastushenko.ivan@gmail.com>
2014-08-07 23:49:51 +0700

    Delete confirm info json page

--- src/main/java/com/ipastushenko/core/web/controller/RegistrationController.java
+++ src/main/java/com/ipastushenko/core/web/controller/RegistrationController.java
@@ -1,7 +1,5 @@
 package com.ipastushenko.core.web.controller;
 
-import com.ipastushenko.core.model.ConfirmInfo;
-import com.ipastushenko.core.model.UserDetailsImpl;
 import com.ipastushenko.core.utils.SessionUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +8,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * registration controller
@@ -35,22 +31,5 @@ public class RegistrationController {
             return new ModelAndView("redirect:/");
         }
         return new ModelAndView("auth/registration");
-    }
-
-    @RequestMapping(value = "/confirm-info.json", method = RequestMethod.GET, headers = "Accept=application/json")
-    public @ResponseBody ConfirmInfo confirmInfo(
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) throws IOException {
-        String env = System.getProperty("env");
-        if (env != null) {
-            if (!env.equals("test")) {
-                response.sendError(404);
-                return null;
-            }
-        }
-
-        //TODO: read confirm information
-        return new ConfirmInfo();
     }
 }
